efb2adb50619b852ae123060210856b0738003de
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-04-23 10:31:00 +0900

    Create3デプロイ前のチェック処理を外部関数化

--- src/deployUpgradeableCreate3.ts
+++ src/deployUpgradeableCreate3.ts
@@ -1,31 +1,21 @@
 import { ethers, upgrades } from "hardhat";
 import { Create3 } from "../typechain-types";
-
 import Create3Meta from "../artifacts/contracts/Create3.sol/Create3.json";
+import { preDeployUpgradeableCreate3 } from "./preDeployUpgradeableCreate3";
 
 /**
  * アップグレード可能なCreate3コントラクトをデプロイします。
  */
 export const deployUpgradeableCreate3 = async ()=> {
     // 事前チェック
-    await verifyProvider();
+    await preDeployUpgradeableCreate3();
 
     const factory = new ethers.ContractFactory(Create3Meta.abi, Create3Meta.bytecode, await ethers.provider.getSigner());
     
-    // const factory = await ethers.getContractFactory("Create3");
-    const create3 = await upgrades.deployProxy(factory, []);
+    // Create3コントラクトのコンストラクタ引数は無し
+    const create3Args = [] as unknown[];
+
+    // Create3コントラクトをデプロイ
+    const create3 = await upgrades.deployProxy(factory, create3Args);
     return (await create3.waitForDeployment()) as unknown as Create3;
 };
-
-const verifyProvider = async () => {
-    // デプロイするアカウントのアドレスを取得
-    const address = (await ethers.provider.getSigner()).address;
-    // デプロイするアカウントのnonceを取得
-    const nonce = await ethers.provider.getTransactionCount(address);
-
-    // 本プロジェクトでは、nonceが0であることを前提としている
-    if (nonce !== 0) {
-        throw new Error("The nonce of the deployer account is not 0. Please use another account.");
-    }
-};
-
