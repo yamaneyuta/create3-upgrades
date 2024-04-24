27d3c0a064d123514348f1deed56c128a5a18119
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-04-24 18:28:44 +0900

    distディレクトリを管理対象外に変更

--- dist/src/getTransparentUpgradeableProxyV5Factory.d.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { Signer } from "ethers";
-/**
- * @openzeppelin-upgrades v5のTransparentUpgradeableProxyFactoryを取得します。
- *
- * `getTransparentUpgradeableProxyFactory`の代わりに使用します。
- */
-export declare const getTransparentUpgradeableProxyV5Factory: (hre: HardhatRuntimeEnvironment, signer?: Signer) => Promise<import("ethers").ContractFactory<any[], import("ethers").BaseContract>>;
--- dist/src/preDeployUpgradeableCreate3.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * `deployUpgradeableCreate3`実行前の事前チェックを行います。
- */
-export declare const preDeployUpgradeableCreate3: () => Promise<void>;
