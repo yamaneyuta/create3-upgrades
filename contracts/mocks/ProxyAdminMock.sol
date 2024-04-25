390b903501a9d7d5b961c0ae4727bb177121941e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-04-25 12:09:49 +0900

    ProxyAdminの定義ファイル(.d.ts)を生成するためだけのコントラクトを追加

---
+++ contracts/mocks/ProxyAdminMock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+
+/**
+ * ProxyAdminのTypeScript定義を作成するためのコントラクト 
+ */
+contract ProxyAdminMock is ProxyAdmin {
+    constructor(address initialOwner) ProxyAdmin(initialOwner) {}
+}
