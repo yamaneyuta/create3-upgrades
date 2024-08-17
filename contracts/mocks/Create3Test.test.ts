291a3ff5012262ea6ec476de32beabf72b4e02a6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-17 15:02:11 +0900

    Create3で同一saltを使用してデプロイする場合、デプロイするウォレットが異なっていても同一アドレスにデプロイされることを確認するテストを追加

--- contracts/mocks/Create3Test.test.ts
+++ contracts/mocks/Create3Test.test.ts
@@ -4,6 +4,7 @@ import { expect } from "chai";
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { Privatenet } from "../../test/lib/Privatenet";
 import { md5 } from "../../test/lib/md5";
+import { Salt } from "../../test/lib/Salt";
 
 describe("[2E4BF335] Deploy with Create3", () => {
     const deployFixture = async () => {
@@ -67,11 +68,31 @@ describe("[2E4BF335] Deploy with Create3", () => {
         // Create3Testのバイトコードチェックのため、Create3コントラクト自体はテストしていないことに注意。
         const impleCode = await provider.getCode(implAddress);
         expect(impleCode.length).to.be.equal(3752);
-        expect(md5(impleCode)).to.be.equal("c3e34db0732f8f0e01b82588511644f3");
+        expect(md5(impleCode)).to.be.equal("f4b94c2da19d61209cf029e1053eab8d");
 
         // proxyCodeはopenzeppelinのProxyコントラクトなので、Create3とは直接関係ないことに注意。
         const proxyCode = await provider.getCode(proxyAddress);
         expect(proxyCode.length).to.be.equal(2320);
         expect(md5(proxyCode)).to.be.equal("639b4e3e55aec99ba6dd1df01f8031d7");
+    });
+
+    it("[CEAE2166] getDeployed()", async () => {
+        let { create3 } = await loadFixture(deployFixture);
+
+        const { deployer, alice } = await Privatenet.signers();
+        const salt = Salt.create("hello");
+
+        const address: string = await create3.getDeployed(salt);
+
+        // deployerがnonce0の時にデプロイした時のCreate3コントラクト(実際はnonce1のProxyコントラクト)を用いて
+        // saltが"hello"の時のアドレスを取得する
+        expect(address).to.be.equal("0xb3E11c333711371378c5A225EE2E3542c1eE2b61");
+
+        // aliceでgetDeployedを実行しても同じアドレスが取得できることを確認
+        // ※別のウォレットでデプロイしても同じアドレスが取得できることは運用上好ましくない場合もあるため注意。
+        create3 = create3.connect(alice) as typeof create3;
+        const address2: string = await create3.getDeployed(salt);
+        expect(address).to.be.equal(address2);
+        expect(address2).to.be.equal("0xb3E11c333711371378c5A225EE2E3542c1eE2b61");
     });
 });
