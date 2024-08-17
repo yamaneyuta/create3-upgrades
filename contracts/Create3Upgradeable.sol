b04c3ad5c5f75c054085087595f068e1da23348c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-17 17:52:27 +0900

    Create3Upgradeableのメソッドにコメントを追加

--- contracts/Create3Upgradeable.sol
+++ contracts/Create3Upgradeable.sol
@@ -11,11 +11,22 @@ abstract contract Create3Upgradeable is Initializable, ContextUpgradeable, ICrea
     // solhint-disable-next-line func-name-mixedcase, no-empty-blocks
     function __Create3_init() internal onlyInitializing { }
 
+    /**
+     * コントラクトをデプロイします。
+     * ※ 別のウォレットからデプロイしたとしても、同一saltの場合は同一のコントラクトアドレスでデプロイされることに注意してください。
+     *    この挙動が好ましくない場合は、以下のような対応を行ってください。
+     *    - 継承先でonlyOwnerなどのアクセス制限を追加する
+     *    - 継承先でこの関数をオーバーライドし、saltをmsg.sender依存にする
+     *      例: salt = keccak256(abi.encodePacked(salt, msg.sender));
+     */
     function _deploy(bytes32 salt, bytes memory creationCode, uint256 value) internal virtual {
         address deployed = CREATE3.deploy(salt, creationCode, value);
         emit Deployed(_msgSender(), salt, deployed);
     }
 
+    /**
+     * このコントラクト経由でデプロイした時のコントラクトアドレスを取得します。
+     */
     function _getDeployed(bytes32 salt) internal virtual view returns (address deployed) {
         deployed = CREATE3.getDeployed(salt);
     }
