46d8fe9b20b9aa2816785f7f493830e71b63576e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-17 17:49:41 +0900

    デプロイするコントラクトに対するProxyの種類を取得するためのクラスを作成

---
+++ src/lib/ProxyKind.ts
@@ -0,0 +1,10 @@
+import hre, { upgrades } from "hardhat";
+import { deployProxyImpl } from "@openzeppelin/hardhat-upgrades/dist/utils";
+import { ContractFactory } from "ethers";
+
+export class ProxyKind {
+    public static async get(implFactory: ContractFactory) {
+        const { kind } = await deployProxyImpl(hre, implFactory, {});
+        return kind;
+    }
+}
